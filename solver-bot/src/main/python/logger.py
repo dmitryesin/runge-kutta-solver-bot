5d081634382889e02c92b8ab53fc563052293c28
Dmitry <dmitryesin@hotmail.com>
2024-05-27 10:28:43 +0300

    Added logging to a file

--- solver-bot/src/main/python/logger.py
+++ solver-bot/src/main/python/logger.py
@@ -1,9 +1,19 @@
 import logging
+import os
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler("logs/bot.log")
+file_handler.setFormatter(formatter)
+
+logging.basicConfig(level=logging.INFO, handlers=[console_handler, file_handler])
+
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
