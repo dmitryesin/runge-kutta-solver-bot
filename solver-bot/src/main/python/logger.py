aeb1b54e493997b6021cfef4e7538403ee34f990
Dmitry <dmitryesin@hotmail.com>
2024-06-25 20:58:36 +0300

    Removed saving logs to a file

--- solver-bot/src/main/python/logger.py
+++ solver-bot/src/main/python/logger.py
@@ -1,20 +1,11 @@
 import logging
-import os
-
-LOGS_DIR = "solver-bot/src/main/python/logs/"
-
-if not os.path.exists(LOGS_DIR):
-    os.makedirs(LOGS_DIR)
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 
-file_handler = logging.FileHandler(LOGS_DIR + "bot.log")
-file_handler.setFormatter(formatter)
-
-logging.basicConfig(level=logging.INFO, handlers=[console_handler, file_handler])
+logging.basicConfig(level=logging.INFO, handlers=[console_handler])
 
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
