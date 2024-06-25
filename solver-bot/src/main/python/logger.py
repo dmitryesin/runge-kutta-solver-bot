a11c8b33b2e78aaf42fae03c69c2a8beed24c3a9
Dmitry <dmitryesin@hotmail.com>
2024-06-25 20:35:37 +0300

    Optimized the path indication

--- solver-bot/src/main/python/logger.py
+++ solver-bot/src/main/python/logger.py
@@ -1,17 +1,17 @@
 import logging
 import os
 
-PY_DIR = "solver-bot/src/main/python/"
+LOGS_DIR = "solver-bot/src/main/python/logs/"
 
-if not os.path.exists(PY_DIR + "logs"):
-    os.makedirs(PY_DIR + "logs")
+if not os.path.exists(LOGS_DIR):
+    os.makedirs(LOGS_DIR)
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 
-file_handler = logging.FileHandler(PY_DIR + "logs/bot.log")
+file_handler = logging.FileHandler(LOGS_DIR + "bot.log")
 file_handler.setFormatter(formatter)
 
 logging.basicConfig(level=logging.INFO, handlers=[console_handler, file_handler])
