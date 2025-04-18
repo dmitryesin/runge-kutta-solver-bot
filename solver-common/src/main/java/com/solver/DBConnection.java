b2473be5383211520f897550a87e6a8af225e04d
Dmitry <dmitryesin@hotmail.com>
2025-04-18 08:52:55 +0300

    Add dotenv support for database configuration in DBConnection class and update pom.xml with java-dotenv dependency.

--- solver-common/src/main/java/com/solver/DBConnection.java
+++ solver-common/src/main/java/com/solver/DBConnection.java
@@ -1,11 +1,21 @@
 package com.solver;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.*;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = String.format(
+        "%s:%s://%s:%s/%s",
+        dotenv.get("DB_DRIVER"),
+        dotenv.get("DB_CONNECTION"),
+        dotenv.get("DB_HOST"),
+        dotenv.get("DB_PORT"),
+        dotenv.get("DB_DATABASE")
+    );
+    private static final String USER = dotenv.get("DB_USERNAME");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
