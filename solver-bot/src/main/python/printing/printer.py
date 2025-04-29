898417aab3d62e876d26787fe58f13975aa7a22f
Dmitry <dmitryesin@hotmail.com>
2025-04-29 17:17:12 +0300

    Refactor print_solution to use format_value for consistent formatting of results

--- solver-bot/src/main/python/printing/printer.py
+++ solver-bot/src/main/python/printing/printer.py
@@ -3,20 +3,30 @@ def print_solution(result, order, rounding):
         order = int(order)
     except ValueError:
         return ""
-
-    superscripts = "⁰¹²³⁴⁵⁶⁷⁸⁹"
+    
+    def format_value(value, rounding):
+        formatted_value = f"{value:.{rounding}f}"
+        if '.' in formatted_value:
+            formatted_value = formatted_value.rstrip('0')
+            if formatted_value.endswith('.'):
+                formatted_value += '0'
+        return formatted_value
 
     def get_variable_name(i):
+        superscripts = "⁰¹²³⁴⁵⁶⁷⁸⁹"
         if i < 4:
             return f"y{chr(39) * i}"
         return f"y{"⁽"}{''.join(superscripts[int(d)] for d in str(i))}{"⁾"}"
 
-    result_str = f"x: {result[0]:.2f}"
+    formatted_x = format_value(result[0], rounding)
+    result_str = f"x: {formatted_x}"
+
     variable_names = ["y"] + [get_variable_name(i) for i in range(1, int(order))]
 
     for i in range(1, order + 1):
         if i < len(result):
-            result_str += f", {variable_names[i-1]}: {result[i]:.{rounding}f}"
+            formatted_y = format_value(result[i], rounding)
+            result_str += f", {variable_names[i-1]}: {formatted_y}"
         else:
             result_str += f", {variable_names[i-1]}: NaN"
 
