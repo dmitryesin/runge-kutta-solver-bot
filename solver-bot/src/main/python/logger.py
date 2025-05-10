cea8f25e667d74a504623668c9aaed9be40cb65a
Dmitry <dmitryesin@hotmail.com>
2025-05-10 18:57:03 +0300

    Refactor logger configuration to correct log directory path and update log file name

--- solver-bot/src/main/python/logger.py
+++ solver-bot/src/main/python/logger.py
@@ -2,15 +2,18 @@ import logging
 
 from pathlib import Path
 
-LOGS_DIR = Path("solver-bot/src/main/python/logs/")
+LOGS_DIR = Path("logs/")
 LOGS_DIR.mkdir(parents=True, exist_ok=True)
 
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 
-file_handler = logging.FileHandler(LOGS_DIR / "bot.log", encoding='utf-8')
+file_handler = logging.FileHandler(LOGS_DIR / "solver-bot.log", encoding='utf-8')
 file_handler.setFormatter(formatter)
 
 logging.basicConfig(level=logging.INFO, handlers=[console_handler, file_handler])
