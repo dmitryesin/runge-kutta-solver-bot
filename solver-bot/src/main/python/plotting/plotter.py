1f55f2e6bd584c4979bc5536ed410123f6960cf3
Dmitry <dmitryesin@hotmail.com>
2025-06-18 19:02:42 +0300

    Add plot_solution_save function

--- solver-bot/src/main/python/plotting/plotter.py
+++ solver-bot/src/main/python/plotting/plotter.py
@@ -1,7 +1,44 @@
 import io
 import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+import plotly.io as pio
 
 from printing.printer import get_variable_name
+
+
+def plot_solution_save(x_values, y_values, order):
+    variable_names = ["y"] + [get_variable_name(i) for i in range(1, order)]
+    fig = go.Figure()
+
+    if isinstance(y_values[0], (list, tuple)):
+        for i, name in enumerate(variable_names):
+            fig.add_trace(go.Scatter(
+                x=x_values,
+                y=[y[i] for y in y_values],
+                mode='lines',
+                name=name
+            ))
+    else:
+        fig.add_trace(go.Scatter(
+            x=x_values,
+            y=y_values,
+            mode='lines',
+            name=variable_names[0]
+        ))
+
+    fig.update_layout(
+        legend=dict(x=0, y=1),
+        width=900,
+        height=600,
+    )
+
+    html_str = pio.to_html(fig, full_html=False, include_plotlyjs='cdn')
+
+    buffer = io.BytesIO()
+    buffer.write(html_str.encode('utf-8'))
+    buffer.seek(0)
+
+    return buffer
 
 
 def plot_solution(x_values, y_values, order):
